87882f38746b54fa3ddc01b0a23aecfb3450069c
moo-nerim <ansgpfla7@naver.com>
2022-06-26 23:12:07 +0900

    [feat] console write

---
+++ C#/HelloCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#"); // 콘솔창에 출력 1) Write() = print() 2) WriteLine() = println()
+            Console.WriteLine("Hello " + args[0]);
+            Console.ReadKey(); 
+        }
+    }
+}
