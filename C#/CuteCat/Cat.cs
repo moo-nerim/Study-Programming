00c7425ab1da5dbcb0262c6192dca0b4a64c51c2
moo-nerim <ansgpfla7@naver.com>
2022-06-29 00:15:46 +0900

    [feat] implement cat class

---
+++ C#/CuteCat/Cat.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CuteCat
+{
+    public class Cat // internal -> CuteCat namespace만 사용 가능 / public -> 외부에서도 사용 가능
+    {
+        public string Name;
+        public int Age;
+
+        private int Happiness = 50;
+
+        public Cat(string name, int age) // 생성자 -> 접근지정자 public, return 타입 x
+        {
+            this.Name = name; // this -> 현재 인스턴스를 가리킴
+            this.Age = age;
+        }
+
+        public void Play()
+        {
+            Happiness += 10;
+
+            if (Happiness > 100)
+            {
+                Happiness = 100;
+            }
+        }
+
+        public void Eat()
+        {
+            Happiness += 20;
+
+            if (Happiness > 100)
+            {
+                Happiness = 100;
+            }
+        }
+
+        public void GetBored()
+        {
+            Happiness -= 10;
+
+            if (Happiness < 0)
+                Happiness = 0;
+        }
+
+        public string Express()
+        {
+            string message = "";
+
+            if (Happiness >= 80)
+                message += "I'm very happy.";
+            else if (Happiness >= 60)
+                message += "I'm happy.";
+            else if (Happiness >= 40)
+                message += "I'm so so.";
+            else if (Happiness >= 20)
+                message += "I'm gloomy.";
+            else
+                message += "I'm sad.";
+
+            return this.Name + " : " + message;
+        }
+    }
+}
---
+++ C#/CuteCat/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuteCat
+{
+    public partial class Form1 : Form
+    {
+        Cat myCat = new Cat("Navi",1);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            myCat.Play();
+            CatScreen.Text = myCat.Express();
+        }
+
+        private void Feed_Click(object sender, EventArgs e)
+        {
+            myCat.Eat();
+            CatScreen.Text = myCat.Express();
+        }
+
+        private void CatTimer_Tick(object sender, EventArgs e) // Timer에서 정한 시간마다 호출
+        {
+            myCat.GetBored();
+            CatScreen.Text = myCat.Express();
+        }
+    }
+}
